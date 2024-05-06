c42c36159105e1ab6069ffcf940e21b915396775
Debbie <deborahhabtom7@gmail.com>
2024-05-06 13:15:13 +0200

    Third commit

--- app/src/main/java/com/rose/mypetapplication/MainActivity2.kt
+++ app/src/main/java/com/rose/mypetapplication/MainActivity2.kt
@@ -1,5 +1,9 @@
 package com.rose.mypetapplication
 
+//ST10460872
+//Deborah Habtom Tesfayohannes
+//YouTube Link: https://youtu.be/Ym4bHN-im-w
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
