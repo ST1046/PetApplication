031a291c67663d007829533bac395c86516b9185
Debbie <deborahhabtom7@gmail.com>
2024-05-06 13:15:13 +0200

    Fourth commit

--- app/src/main/java/com/rose/mypetapplication/MainActivity.kt
+++ app/src/main/java/com/rose/mypetapplication/MainActivity.kt
@@ -10,9 +10,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-private lateinit var startBtn:Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var startBtn:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/rose/mypetapplication/MainActivity3.kt
+++ app/src/main/java/com/rose/mypetapplication/MainActivity3.kt
@@ -9,10 +9,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-private lateinit var dogBtn: Button
-private lateinit var catBtn:Button
-
 class MainActivity3 : AppCompatActivity() {
+
+    private lateinit var dogBtn: Button
+    private lateinit var catBtn:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
